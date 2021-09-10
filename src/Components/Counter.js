582e1d54becddeb34ac0f64f25bd83e779e930a7
KnutAaboe <knutandersaaboe@gmail.com>
2021-09-10 16:04:47 +0200

    Cleanup

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -2,13 +2,10 @@ import React, {useState} from 'react'
 
 function Counter() {
     const [count, setCounter] = useState(0)
-    incrementClick = () => {
-        
-    }
     return (
         <div>
             <p>{count}</p>
-            <span><button onClick={() => setCounter(count + 1)}></button></span>
+            <span><button onClick={() => setCounter(count + 1)}>click me</button></span>
         </div>
     )
 }
